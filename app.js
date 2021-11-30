f554ec669fa3116e18c838cc8dceee39656cdcb3
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2021-11-30 17:12:31 +0200

    finish

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 import { upload } from './upload';
-console.log('app.js');
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcMFqqXUjOyjhe5NnfHK_KcOYK3rRkqek",
+  authDomain: "fe-upload-c9397.firebaseapp.com",
+  projectId: "fe-upload-c9397",
+  storageBucket: "fe-upload-c9397.appspot.com",
+  messagingSenderId: "1063373761265",
+  appId: "1:1063373761265:web:5666ad98c9f3fa4fec5750"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage();
+
 upload('#file', {
   multi: true,
-  accept: ['.png', '.jpg','.jpeg','.gif']
+  accept: ['.png', '.jpg','.jpeg','.gif'],
+  onUpload(files, blocks) {
+    files.forEach((file, index) => {
+      const metadata = {
+        contentType: 'image/jpeg'
+      };
+      const storageRef = ref(storage, 'images/' + file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+      
+      uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        const block = blocks[index].querySelector('.preview-info-progress');
+        block.textContent = `${+progress}%`;
+        block.style.width = `${progress}%`;
+      },
+      (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+            break;
+            case 'storage/canceled':
+              // User canceled the upload
+            break;
+            // ...
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+            }
+      }, 
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
+      }
+      );
+    })
+  }
 });
--- upload.js
+++ upload.js
@@ -5,15 +5,26 @@ function bytesToSize(bytes) {
   return Math.round(bytes / Math.pow(1024, i)) + '' + sizes[i];
 }
 
+const element = (tag, classes =[], content) => {
+  const node = document.createElement(tag);
+
+  if(classes.length) node.classList.add(...classes);
+
+  if(content) node.textContent = content;
+
+  return node;
+}
+
+function noop() {}
+
 export function upload(selector, options = {}) {
+  let files = [];
+  const onUpload = options.onUpload ?? noop;
   const input = document.querySelector(selector);
-  const preview = document.createElement('div');
-
-  preview.classList.add('preview');
-
-  const open = document.createElement('button');
-  open.classList.add('btn');
-  open.textContent = 'Open';
+  const preview = element('div', ['preview'])
+  const open = element('button', ['btn'], 'Open');
+  const upload = element('button', ['btn', 'primary'], 'Download');
+  upload.style.display = 'none';
 
   if (options.multi) {
     input.setAttribute('multiple', true);
@@ -24,16 +35,19 @@ export function upload(selector, options = {}) {
   }
 
   input.insertAdjacentElement('afterend', preview );
+  input.insertAdjacentElement('afterend', upload );
   input.insertAdjacentElement('afterend', open );
 
   const triggerInput = () => input.click();
+  
 
   const changeHandler = event => {
     if (!event.target.files.length) return
 
-    const files  =  Array.from(event.target.files);
+    files = Array.from(event.target.files);
 
     preview.innerHTML = '';
+    upload.style.display = 'inline';
 
     files.forEach( file => {
       if(!file.type.match('image')) return
@@ -44,11 +58,11 @@ export function upload(selector, options = {}) {
         const src = event.target.result;
         preview.insertAdjacentHTML('afterbegin', `
         <div class="preview-image"/>
-        <div class="preview-remove">&times;</div>
+          <div class="preview-remove" data-name="${file.name}">&times;</div>
           <img src="${src}" alt="${file.name}"/>
           <div class="preview-info">
             <span>${file.name}</span>
-            ${file.size}
+            ${bytesToSize(file.size)}
           </div>
         </div>
         `);
@@ -56,9 +70,38 @@ export function upload(selector, options = {}) {
 
       reader.readAsDataURL(file);
     })
-
   }
+
+  const removeHandler = (event) => {
+    if(!event.target.dataset.name) return
+
+    const {name} = event.target.dataset;
+    
+    files = files.filter(file => file.name !== name);
+
+    if (!files.length) upload.style.display = 'none';
+
+    const block = preview.querySelector(`[data-name="${name}"]`).closest('.preview-image');
+    
+    block.classList.add('removing');
+
+    setTimeout(() =>  block.remove(), 300);
+  }
+
+  const clearPreview = el => {
+    el.style.bottom = `4px`;
+    el.innerHTML = '<div class="preview-info-progress"></div>';
+  }
+
+  const uploadHandler = () => {
+    preview.querySelectorAll('.preview-remove').forEach((e) => e.remove());
+    const previewInfo = preview.querySelectorAll('.preview-info ');
+    previewInfo.forEach(clearPreview);
+    onUpload(files, previewInfo)
+  };
 
   open.addEventListener('click', triggerInput);
   input.addEventListener('change', changeHandler);
+  preview.addEventListener('click',removeHandler);
+  upload.addEventListener('click', uploadHandler)
 }
